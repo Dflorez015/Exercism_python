df7c62f18351c194646c951586fcab6257c0155f
Df Máximo <duvanflorez013@gmail.com>
2020-03-05 13:37:43 -0500

    Segunda version

--- acronym/acronym.py
+++ acronym/acronym.py
@@ -4,6 +4,5 @@ def abbreviate(words):
   words = words.replace("-"," ")
   words = words.replace(",","")
   words = words.replace("'","")
-  print(words)
   acronym = ''.join(word[0] for word in words.upper().split())
   return acronym

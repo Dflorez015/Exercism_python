8e790c9b9b953f49a313ce4348c9583cd87631a2
Df Máximo <duvanflorez013@gmail.com>
2020-03-05 13:35:49 -0500

    Quinto ejercicio

---
+++ word-count/word_count.py
@@ -0,0 +1,18 @@
+"""
+def count_words(sentence):    
+    sentence.replace("\n","")
+    words = list(sentence.casefold().split())
+    return {x: words.count(x) for x in words}
+"""
+def count_words(sentence):    
+    signs = "123456789abcdefghijklmnñopqrstuvwxyzABCDEFGHIJKLMNÑOPQRSTUVWXYZ' "
+    for x in sentence:
+        if signs.count(x) == 0:         
+            sentence = sentence.replace(x," ")
+        elif sentence.count("don't") <= 1:
+            if sentence.count("'") > 1:               
+                    sentence = sentence.replace("'"," ")      
+    sentence = sentence.replace("can t", "can't")
+    words = list(sentence.casefold().split())
+    return {x: words.count(x) for x in words}
+
